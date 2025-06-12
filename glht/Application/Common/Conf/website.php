c84c697df9a090775d6632634da3e8cc7326e573
wuhaifei215 <wuhaifei215@gmail.com>
2025-06-12 17:41:42 +0800

    Update website.php

--- glht/Application/Common/Conf/website.php
+++ glht/Application/Common/Conf/website.php
@@ -3,7 +3,7 @@
 		'WEB_TITLE' => 'DH国际支付',
 		'DOMAIN' => 'hx.dhpay.vip',
 		'MODULE_ALLOW_LIST'   => array('Home','User','hxadmin','hxadminpak','Install', 'Weixin','Pay','Cashier','Agent','Payment','Cli'),
-		'URL_MODULE_MAP'  => array('hxadmin'=>'admin','hxadminpak'=>'adminpak', 'agent'=>'user', 'user'=>'user'),
+		'URL_MODULE_MAP'  => array('hxadmin'=>'admin','hxadminpak'=>'adminPAK', 'agent'=>'user', 'user'=>'user'),
 		'LOGINNAME' => 'user',
 		'HOUTAINAME' => 'hxadmin',
 		'API_DOMAIN' => 'api.dhpay.vip',

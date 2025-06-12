70b5d9919efa1efa726dc347285db479c67ac57a
wuhaifei215 <wuhaifei215@gmail.com>
2025-06-12 21:08:39 +0800

    Update db.php

--- glht/Application/Common/Conf/db.php
+++ glht/Application/Common/Conf/db.php
@@ -20,7 +20,7 @@ return array(
     'REDIS_PWD'   => 'Yun!x@y#z99pay',       //密码
     'REDIS_SELECT' => '0',
 
-    'SESSION_TYPE' => 'Db',
+    'SESSION_TYPE' => 'Mysqli',
     'SESSION_TABLE' => '',
     'DB_PREFIX' => 'think_',
 

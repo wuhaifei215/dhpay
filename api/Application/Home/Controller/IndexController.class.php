9601501d5e4f71f63f76782657e81c476fe3bd57
wuhaifei215 <wuhaifei215@gmail.com>
2025-06-12 17:24:51 +0800

    Update IndexController.class.php

--- api/Application/Home/Controller/IndexController.class.php
+++ api/Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,9 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+        // 获取服务器IP
+        $serverIP = $_SERVER['SERVER_ADDR'];
+        echo $serverIP;
         $this->show('请使用正确的接口地址！','utf-8');
     }
 }

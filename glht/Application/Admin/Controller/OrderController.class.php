d633509a9a68b4825b2e44986b6b6bd3d1aec6ba
wuhaifei215 <wuhaifei215@gmail.com>
2025-06-13 15:03:34 +0800

    1

--- glht/Application/Admin/Controller/OrderController.class.php
+++ glht/Application/Admin/Controller/OrderController.class.php
@@ -538,7 +538,6 @@ class OrderController extends BaseController
         $count = $MoneychangeModel->getCount($where);
         $page = new Page($count, $rows);
         $list = $MoneychangeModel->getOrderByDateRange('*', $where, $page->firstRow . ',' . $page->listRows, 'id desc');
-var_dump($where);
         if ($bank == 9) {
             //总佣金笔数
             $stat['totalcount'] = $MoneychangeModel->getCount($where);

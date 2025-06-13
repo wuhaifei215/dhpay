c06a3b3ef32408b054cf445eeb2fd7794f6c50ee
wuhaifei215 <wuhaifei215@gmail.com>
2025-06-13 15:08:55 +0800

    Update OrderController.class.php

--- glht/Application/Admin/Controller/OrderController.class.php
+++ glht/Application/Admin/Controller/OrderController.class.php
@@ -492,9 +492,9 @@ class OrderController extends BaseController
 
         $where = array();
         $currency = I("request.currency", '', 'trim,string,strip_tags,htmlspecialchars');
-        if ($currency) {
-            $where['currency'] = ['eq', $currency];
-        }
+//        if ($currency) {
+        $where['currency'] = ['eq', $currency];
+//        }
         $this->assign('currency', $currency);
 
         $memberid = I("get.memberid", 0, 'intval');
